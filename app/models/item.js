9e3084db589d32ae23fcb14ac055ccc4676d667c
Gary Hopkins <gary287@gmail.com>
2022-01-06 11:16:53 -0500

    fix lint errors

--- app/models/item.js
+++ app/models/item.js
@@ -1,10 +1,10 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class ItemModel extends Model {
-    @attr db_id
-    @attr title
-    @attr url
-    @attr wish_list_id
-    @attr created_at
-    @attr updated_at
+  @attr db_id;
+  @attr title;
+  @attr url;
+  @attr wish_list_id;
+  @attr created_at;
+  @attr updated_at;
 }
--- app/models/wishlist.js
+++ app/models/wishlist.js
@@ -1,9 +1,9 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class WishlistModel extends Model {
-    @attr db_id;
-    @attr title;
-    @attr created_at;
-    @attr updated_at;
-    @attr shared_id;
+  @attr db_id;
+  @attr title;
+  @attr created_at;
+  @attr updated_at;
+  @attr shared_id;
 }
