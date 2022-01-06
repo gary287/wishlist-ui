3228c561101550750fcd5d9a3abfcd59c3bca774
Gary Hopkins <gary287@gmail.com>
2022-01-06 06:10:42 -0500

    add models

---
+++ app/models/item.js
@@ -0,0 +1,10 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class ItemModel extends Model {
+    @attr db_id
+    @attr title
+    @attr url
+    @attr wish_list_id
+    @attr created_at
+    @attr updated_at
+}
---
+++ app/models/wishlist.js
@@ -0,0 +1,9 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class WishlistModel extends Model {
+    @attr db_id;
+    @attr title;
+    @attr created_at;
+    @attr updated_at;
+    @attr shared_id;
+}
